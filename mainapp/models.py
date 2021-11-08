ea75519a29b543e57ad183523097eb12d131fe1c
Uryubdzhirov <89509508+Uryubdzhirov@users.noreply.github.com>
2021-11-08 19:57:29 +0300

    Lesson3. Code style fix

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class ProductCategory(models.Model):
     name = models.CharField(verbose_name="имя", max_length=64, unique=True)
     description = models.TextField(verbose_name="описание", blank=True)
