099e8861729aa110175d29f4a1ed683e2f7976b0
Uryubdzhirov <Uryubdzhirov@gmail.com>
2021-12-13 17:14:38 +0000

    fix

--- authnapp/forms.py
+++ authnapp/forms.py
@@ -1,10 +1,7 @@
-import hashlib
-import random
-
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, UserCreationForm
 
-from .models import ShopUser, ShopUserProfile
+from .models import ShopUser
 
 
 class ShopUserLoginForm(AuthenticationForm):
@@ -31,16 +28,6 @@ class ShopUserRegisterForm(UserCreationForm):
             raise forms.ValidationError("Вы слишком молоды!")
         return data
 
-    def save(self):
-        user = super(ShopUserRegisterForm, self).save()
-
-        user.is_active = False
-        salt = hashlib.sha1(str(random.random()).encode("utf8")).hexdigest()[:6]
-        user.activation_key = hashlib.sha1((user.email + salt).encode("utf8")).hexdigest()
-        user.save()
-
-        return user
-
     class Meta:
         model = ShopUser
         fields = ("username", "first_name", "password1", "password2", "email", "age", "avatar")
@@ -63,14 +50,3 @@ class ShopUserEditForm(UserChangeForm):
     class Meta:
         model = ShopUser
         fields = ("username", "first_name", "email", "age", "avatar")
-
-
-class ShopUserProfileEditForm(forms.ModelForm):
-    class Meta:
-        model = ShopUserProfile
-        fields = ("tagline", "aboutMe", "gender")
-
-    def __init__(self, *args, **kwargs):
-        super(ShopUserProfileEditForm, self).__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs["class"] = "form-control"
