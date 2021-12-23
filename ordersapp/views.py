06d1d139b8f44d3b4eabc8674120e4f0dda204d3
Uryubdzhirov <Uryubdzhirov@gmail.com>
2021-12-23 15:19:01 +0000

    lesson_16. F objects

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -12,6 +12,7 @@ from basketapp.models import Basket
 from ordersapp.forms import OrderItemForm
 from ordersapp.models import Order, OrderItem
 
+from django.db.models import F
 
 class OrderList(LoginRequiredMixin, ListView):
     model = Order
@@ -129,20 +130,17 @@ def order_forming_complete(request, pk):
 @receiver(pre_save, sender=Basket)
 def product_quantity_update_save(instance, sender, **kwargs):
     if instance.pk:
-        """If user change quantity in order or basket"""
-        instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
+        instance.product.quantity = F("quantity") - (instance.quantity - sender.get_item(instance.pk).quantity)
     else:
-        """If user create order or basket"""
-        instance.product.quantity -= instance.quantity
+        instance.product.quantity = F("quantity") - instance.quantity
     instance.product.save()
 
 
 @receiver(pre_delete, sender=OrderItem)
 @receiver(pre_delete, sender=Basket)
 def product_quantity_update_delete(instance, **kwargs):
-    instance.product.quantity += instance.quantity
+    instance.product.quantity = F("quantity") + instance.quantity
     instance.product.save()
-
 
 from django.http import JsonResponse
 
